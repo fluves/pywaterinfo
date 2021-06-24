03308c7ded26ff933b920fc0561abe649377416f
Stijn Van Hoey <stijn@fluves.com>
2021-06-24 15:34:20 +0200

    Pyscaffold update with manual review

--- src/pywaterinfo/__init__.py
+++ src/pywaterinfo/__init__.py
@@ -1,21 +1,20 @@
-# -*- coding: utf-8 -*-
-"""
-The :class:`pywaterinfo.Waterinfo` is the main
-class for waterinfo.be data query interactions.
-"""
-
-from pkg_resources import DistributionNotFound, get_distribution
+import sys
 
 from .waterinfo import HIC_BASE, VMM_BASE, Waterinfo
+
+if sys.version_info[:2] >= (3, 8):
+    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
+    from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
+else:
+    from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
 
 try:
     # Change here if project is renamed and does not equal the package name
     dist_name = __name__
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
+    __version__ = version(dist_name)
+except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
 finally:
-    del get_distribution, DistributionNotFound
-
+    del version, PackageNotFoundError
 
 __all__ = ["HIC_BASE", "VMM_BASE", "Waterinfo"]
