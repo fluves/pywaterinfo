807a82b12cba377be92f356ae49182cfbc0b6bb7
stijnvanhoey <stijnvanhoey@users.noreply.github.com>
2021-07-02 13:21:06 +0000

    deploy: c7b3856692edaa994f5e9fe94b9e445adf619212

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.0.post1.dev13+ge26cf01',
+    VERSION: '0.4.0.post1.dev21+gc7b3856',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
